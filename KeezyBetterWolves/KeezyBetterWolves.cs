01add5960aeadcc9ddafb00706a9264798287179
Marq Gray <marquis.s.sandifer@gmail.com>
2021-03-01 17:21:28 -0600

    Refactored and added a greeting message

--- KeezyBetterWolves/KeezyBetterWolves.cs
+++ KeezyBetterWolves/KeezyBetterWolves.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using BepInEx;
 using HarmonyLib;
+using UnityEngine;
 
 namespace KeezyBetterWolves
 {
     [BepInPlugin("KeezyBetterWolves", "Keezy's Better Wolves", "0.1.0.0")]
     public class KeezyBetterWolves
     {
+        private void Awake()
+        {
+            Debug.Log("Keezy's Better Wolves 0.1.0.0");
+        }
+
         [HarmonyPatch(typeof(Character), "Awake")]
         [HarmonyPostfix]
-        static bool CharacterAwakePatch()
+        private static bool CharacterAwakePatch()
         {
             throw new NotImplementedException();
         }
-        
     }
 }
