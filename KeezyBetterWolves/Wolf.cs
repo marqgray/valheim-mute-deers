54e20c1863eabd33ae0d51189a21a2df8add1853
Marq Gray <marquis.s.sandifer@gmail.com>
2021-03-01 15:33:03 -0600

    Added Mute state methods

--- KeezyBetterWolves/Wolf.cs
+++ KeezyBetterWolves/Wolf.cs
@@ -5,11 +5,49 @@ namespace KeezyBetterWolves
     public class Wolf
     {
         public Character Character { get; }
+        private readonly float _originalIdleSoundChance;
+        private readonly float _originalIdleSoundInterval;
+        private const float MutedFloatValue = 0f;
 
-        public bool Mute()
+        public Wolf()
         {
             throw new NotImplementedException();
-            return true;
+            this._originalIdleSoundChance = this.Character.GetBaseAI().m_idleSoundChance;
+            this._originalIdleSoundInterval = this.Character.GetBaseAI().m_idleSoundInterval;
+        }
+
+        public void SetMuteState(bool isMuted)
+        {
+            try
+            {
+                if (isMuted)
+                {
+                    this.Character.GetBaseAI().m_idleSoundChance = MutedFloatValue;
+                    this.Character.GetBaseAI().m_idleSoundInterval = MutedFloatValue;
+                }
+                else
+                {
+                    this.Character.GetBaseAI().m_idleSoundChance = this._originalIdleSoundChance;
+                    this.Character.GetBaseAI().m_idleSoundInterval = this._originalIdleSoundInterval;
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool GetMuteState()
+        {
+            try
+            {
+                return Math.Abs(this.Character.GetBaseAI().m_idleSoundChance - MutedFloatValue) < 0.1f &&
+                       Math.Abs(this.Character.GetBaseAI().m_idleSoundInterval - MutedFloatValue) < 0.1f;
+            }
+            catch (Exception e)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
