218a100f265e6069e22162aa4444c444b7e4ef33
Marq Gray <marquis.s.sandifer@gmail.com>
2021-03-01 15:04:41 -0600

    Added "MuteWolf" method to be constructed

--- KeezyBetterWolves/KeezyBetterWolves.cs
+++ KeezyBetterWolves/KeezyBetterWolves.cs
@@ -1,9 +1,15 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 
 namespace KeezyBetterWolves
 {
     [BepInPlugin("KeezyBetterWolves", "Keezy's Better Wolves", "0.1.0.0")]
     public class KeezyBetterWolves
     {
+        public bool MuteWolf()
+        {
+            throw new NotImplementedException();
+            return true;
+        }
     }
 }
