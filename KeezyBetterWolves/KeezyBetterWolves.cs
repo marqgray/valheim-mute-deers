9b11b1a948ad5cb714fc43a7bcd36fa98878931c
Marq Gray <marquis.s.sandifer@gmail.com>
2021-03-01 15:56:41 -0600

    Added base patcher for wolf spawns

--- KeezyBetterWolves/KeezyBetterWolves.cs
+++ KeezyBetterWolves/KeezyBetterWolves.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using BepInEx;
+using HarmonyLib;
 
 namespace KeezyBetterWolves
 {
     [BepInPlugin("KeezyBetterWolves", "Keezy's Better Wolves", "0.1.0.0")]
     public class KeezyBetterWolves
     {
+        [HarmonyPatch(typeof(SpawnSystem.SpawnData))]
+        [HarmonyPatch(new[] {typeof(SpawnSystem.SpawnData)})]
+        [HarmonyPostfix]
+        static bool WolfSpawnPatch()
+        {
+            throw new NotImplementedException();
+        }
         
     }
 }
