3d4c0cf3b5d5845e38a931b7333a219975e72e65
Marq Gray <marquis.s.sandifer@gmail.com>
2021-03-02 16:28:57 -0600

    Added sound play delegate and patches

--- KeezyBetterWolves/KeezyBetterWolves.cs
+++ KeezyBetterWolves/KeezyBetterWolves.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 namespace KeezyBetterWolves
 {
     public delegate void CharacterAwakeListener(Character character);
+    public delegate void ZSFXPlayListener(ZSFX sfx, ref bool shouldMute);
 
     [BepInPlugin("KeezyBetterWolves", "Keezy's Better Wolves", "0.1.0.0")]
     public class KeezyBetterWolves : BaseUnityPlugin
@@ -15,6 +16,7 @@ namespace KeezyBetterWolves
         }
 
         public static event CharacterAwakeListener CharacterAwakeEvent;
+        public static event ZSFXPlayListener ZSFXPlayEvent;
 
         [HarmonyPatch(typeof(Character), "Awake")]
         private class CharacterAwakePatch
@@ -23,6 +25,18 @@ namespace KeezyBetterWolves
             private static void Postfix(ref Character __instance)
             {
                 CharacterAwakeEvent?.Invoke(__instance);
+            }
+        }
+        
+        [HarmonyPatch(typeof(ZSFX), "Play")]
+        private class ZSFXPlayPatch
+        {
+            [HarmonyPrefix]
+            private static bool Prefix(ref ZSFX __instance)
+            {
+                bool shouldMute = false;
+                ZSFXPlayEvent.Invoke(__instance, ref shouldMute);
+                return !shouldMute;
             }
         }
     }
