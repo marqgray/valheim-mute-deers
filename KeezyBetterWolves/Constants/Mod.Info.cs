351dc556edec654ff55388d69fc5a1bed756dc0a
Marq Gray <marquis.s.sandifer@gmail.com>
2021-03-04 12:34:44 -0600

    Changed version number

--- KeezyBetterWolves/Constants/Mod.Info.cs
+++ KeezyBetterWolves/Constants/Mod.Info.cs
@@ -3,6 +3,6 @@
     public static class ModInfo
     {
         public const string Name = "Keezy's Better Wolves";
-        public const string Version = "0.2.0.0";
+        public const string Version = "0.2.1.0";
     }
 }
